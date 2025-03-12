5ec5680b759e21612c9dae7cc38c0e32278e53db
clubboy123 <clubboy123@gmail.com>
2025-03-12 17:03:10 +0800

    add first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Tests First Post",
+        "excerpt":"defaults:   # _posts     scope:   path: “”   type: posts values:   layout: single   author_profile: true   read_time: true   comments: true   share: true   related: true  ","categories": [],
+        "tags": [],
+        "url": "/tests-first-post/",
+        "teaser": null
+      }]
